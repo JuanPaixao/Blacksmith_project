54e6f145ee0a71fd53e8f5f34350662e86e8a7b5
Juan Paixao <juanpaixao@live.com>
2019-10-08 16:11:45 -0300

    base mechanics (clients order)

---
+++ Assets/Scripts/Customers.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Customers : MonoBehaviour
+{
+    public Sprite[] sprites;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        int spriteSelector = Random.Range(0, sprites.Length);
+        _spriteRenderer.sprite = sprites[spriteSelector];
+    }
+}
