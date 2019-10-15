47cc64a38e101d51ff16ec05b8aa8ce42edcc0fd
Juan Paixao <juanpaixao@live.com>
2019-10-14 21:27:00 -0300

    first test developed with cooking mechanics

---
+++ Assets/Scripts/BinControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class BinControl : MonoBehaviour
+{
+    public Transform bottomBreadObj, topBreadObj, burgerObj, backHotDogBreadObj, topHotDogBreadObj, hotDogObj;
+
+    private void OnMouseDown()
+    {
+        if (gameObject.name == "bread")
+        {
+            if (Gameplay.cuttingBoardS1 == "empty")
+            {
+                Instantiate(bottomBreadObj, new Vector2(-0.6f, -0.5f), Quaternion.identity);
+                Instantiate(topBreadObj, new Vector2(-0.6f, -0.4f), Quaternion.identity);
+                Gameplay.cuttingBoardS1 = "full";
+            }
+            else if (Gameplay.cuttingBoardS2 == "empty")
+            {
+                Instantiate(bottomBreadObj, new Vector2(0.5f, -0.5f), Quaternion.identity);
+                Instantiate(topBreadObj, new Vector2(0.5f, -0.4f), Quaternion.identity);
+                Gameplay.cuttingBoardS2 = "full";
+            }
+        }
+
+        if (gameObject.name == "hamburgers")
+        {
+            if (Gameplay.grillS1 == "empty")
+            {
+                Instantiate(burgerObj, new Vector2(-7.45f, -1.10f), Quaternion.identity);
+                Gameplay.grillS1 = "full";
+            }
+            else if (Gameplay.grillS2 == "empty")
+            {
+                Instantiate(burgerObj, new Vector2(-5.75f, -1.10f), Quaternion.identity);
+                Gameplay.grillS2 = "full";
+            }
+        }
+    }
+}
